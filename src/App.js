eccd90d442fe4ed820fcc648912ecf815a2e04c6
sam1690 <engg.smishra@gmail.com>
2019-01-07 13:49:09 +0530

    social login fb and gmail

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { GoogleLogin } from 'react-google-login';
+
+import FacebookLogin from 'react-facebook-login';
 
 class App extends Component {
   render() {
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
+
+    const responseFacebook = (response) => {
+      console.log(response);
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <GoogleLogin
+    clientId="365233749389-fpq28ljap69pavj1ts5ntvs6k44p1mk0.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+  />
+        
+        
+  <FacebookLogin
+    appId="629812180770646"
+    autoLoad={true}
+    fields="name,email,picture"
+    
+    
+    callback={responseFacebook} />
+
       </div>
     );
   }
 }
 
 export default App;
+
+
+
